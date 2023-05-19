0ee8a1f3b07b56dff6490d1bf1da7ffb79f15bb0
FedeCrossetto <fede.vazquez.crossetto@gmail.com>
2023-05-19 18:23:20 -0300

    Add src folder

---
+++ src/N5.Api/IRepository/IPermisoRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using N5.Api.Models;
+
+namespace N5.Api.IRepository
+{
+    public interface IPermisoRepository
+    {
+        Task<List<Permiso>> GetAll();
+        Task<Permiso> GetById(int id);
+        Task Add(Permiso permiso);
+        Task Update(Permiso permiso);
+        Task Delete(int id);
+        bool Exists(int id);
+    }
+}
---
+++ src/N5.Api/Repository/PermisoRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N5.Api.IRepository;
+using N5.Api.Models;
+
+namespace N5.Api.Repository
+{
+    public class PermisoRepository : IPermisoRepository
+    {
+        private readonly N5Context _context;
+
+        public PermisoRepository(N5Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Permiso>> GetAll()
+        {
+            return await _context.Permisos
+                .Include(p => p.TipoPermisoNavigation)
+                .ToListAsync();
+        }
+
+        public async Task<Permiso> GetById(int id)
+        {
+            return await _context.Permisos
+                .Include(p => p.TipoPermisoNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task Add(Permiso permiso)
+        {
+            _context.Permisos.Add(permiso);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Permiso permiso)
+        {
+            _context.Update(permiso);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var permiso = await _context.Permisos.FindAsync(id);
+            if (permiso != null)
+            {
+                _context.Permisos.Remove(permiso);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public bool Exists(int id)
+        {
+            return _context.Permisos.Any(e => e.Id == id);
+        }
+    }
+}
